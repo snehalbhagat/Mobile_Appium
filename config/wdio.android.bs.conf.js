29d8674c9730c1dff6b3c0a27e997413d1b3653b
Snehal Bhagat <snehal.d.bhagat@gmail.com>
2022-11-11 16:05:44 +0530

    first commit

---
+++ config/wdio.android.bs.conf.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const { config } = require('./wdio.shared.conf');
+
+//
+// ============
+// BrowserStack Credentials
+// ============
+config.user = process.env.BROWSERSTACK_USER;
+config.key = process.env.BROWSERSTACK_KEY;
+
+//
+// ============
+// Specs
+// ============
+config.specs = [
+  path.join(process.cwd(), './test/specs/android/add-note-screen*.js')
+];
+
+//
+// ============
+// Capabilities
+// ============
+config.capabilities = [
+  {
+    platformName: "Android",
+    "appium:platformVersion": "10.0",
+    "appium:deviceName": "Google Pixel 3",
+    "appium:automationName": "UIAutomator2",
+    "appium:app": "bs://127e61f6e7c08b32d2f92ef557abee1432abf203",
+    "appium:autoGrantPermissions": true
+  }
+]
+
+//
+// Test runner services
+// Services take over a specific job you don't want to take care of. They enhance
+// your test setup with almost no effort. Unlike plugins, they don't add new
+// commands. Instead, they hook themselves up into the test process.
+config.services = ['browserstack'];
+
+exports.config = config;
